a2832e00924503469570ee91409e0f9a10bc1626
cutsea110 <cutsea110@gmail.com>
2021-06-06 14:52:18 +0900

    why?

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
-fn apply<F>(f: F)
-where
-    F: Fn(),
-{
-    f();
+fn create_fn() -> impl Fn() {
+    let text = "Fn".to_owned();
+
+    move || println!("This is a: {}", text)
+}
+
+fn create_fnmut() -> impl FnMut() {
+    let text = "FnMut".to_owned();
+
+    move || println!("This is a: {}", text)
+}
+
+fn create_fnonce() -> impl FnOnce() {
+    let text = "FnOnce".to_owned();
+
+    move || println!("This is a: {}", text)
 }
 
 fn main() {
-    let x = 7;
-    let print = || println!("{}", x);
-    apply(print);
+    let fn_plain = create_fn();
+    let mut fn_mut = create_fnmut();
+    let fn_once = create_fnonce();
+
+    fn_plain();
+    fn_mut();
+    fn_once();
 }
