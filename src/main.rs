f8e4d0990e04f3c46dee4f9f5bb9861dbb92f72a
cutsea110 <cutsea110@gmail.com>
2021-06-06 14:45:41 +0900

    ref.) http://doc.rust-jp.rs/rust-by-example-ja/fn/closures/anonymity.html

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,12 @@
-use std::thread;
-use std::time::Duration;
-
-fn add1(x: u32) -> u32 {
-    x + 1
-}
-
-fn double(x: u32) -> u32 {
-    x + x
-}
-
-fn twice<T>(f: impl Fn(T) -> T) -> impl Fn(T) -> T {
-    move |x| f(f(x))
-}
-
-struct Cacher<T, F>
+fn apply<F>(f: F)
 where
-    F: Fn(T) -> T,
+    F: Fn(),
 {
-    calculation: F,
-    value: Option<T>,
-}
-impl<T, F> Cacher<T, F>
-where
-    T: Copy,
-    F: Fn(T) -> T,
-{
-    fn new(calculation: F) -> Cacher<T, F> {
-        Cacher {
-            calculation,
-            value: None,
-        }
-    }
-
-    fn value(&mut self, arg: T) -> T {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
-    }
+    f();
 }
 
 fn main() {
-    println!("Hello, world!");
-
-    let x = vec![1, 2, 3];
-    let eq_to_x = |z| z == x;
-    println!("{:?}", x);
-    let y = vec![1, 2, 3];
-    assert!(eq_to_x(y));
-
-    let v1 = vec![1, 2, 3, 4, 5];
-    let v1_iter = v1.iter();
-    let total: i32 = v1_iter.sum();
-    assert_eq!(total, 15);
-    let v1_iter = v1.iter();
-    for v in v1_iter {
-        println!("{:?}", v);
-    }
-
-    println!("twice with add1 for 4: {}", twice(add1)(4));
-    println!("twice with double for 4: {}", twice(double)(4));
-
-    let dbl = |x: f32| -> f32 { x + x };
-
-    println!("twice with double for 4.0: {}", twice(dbl)(4.0));
-
-    let mut expensive_result = Cacher::new(|num: u32| -> u32 {
-        println!("calculationg slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    });
-
-    println!("1st time: {}", expensive_result.value(12));
-    println!("2nd time: {}", expensive_result.value(12));
+    let x = 7;
+    let print = || println!("{}", x);
+    apply(print);
 }
