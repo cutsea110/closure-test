a2d086b682ecb8ef8f19c5a5b7a1eab1729baadc
cutsea110 <cutsea110@gmail.com>
2021-06-11 07:52:09 +0900

    try async

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io::Error;
+use std::thread;
+
 fn add_n(n: u32) -> impl Fn(u32) -> u32 {
     move |x| x + n
 }
@@ -20,8 +23,23 @@ where
     f(f(x))
 }
 
+async fn async_add_n(n: u32) -> impl Fn(u32) -> u32 {
+    move |x| x + n
+}
+
+async fn async_add1(x: u32) -> Result<u32, Error> {
+    let f = async_add_n(1).await;
+    Ok(f(x))
+}
+
 fn main() {
     println!("{}", add1(3));
     println!("{}", (twice(Box::new(add1)))(3));
     println!("{}", (apply_twice(add1, 3)));
+
+    let child = thread::spawn(move || add1(7));
+    match child.join() {
+        Ok(n) => println!("{}", n),
+        Err(e) => println!("error: {:?}", e),
+    }
 }
